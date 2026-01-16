c34d3dc561e84639caca5a35275005cdb128defe
Takeshi Nomura <nomuratakeshi24@gmail.com>
2026-01-16 21:53:45 +0900

    Fix: settings.py

--- image_classification/settings.py
+++ image_classification/settings.py
@@ -93,7 +93,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # 本番用の集約フォルダ
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
